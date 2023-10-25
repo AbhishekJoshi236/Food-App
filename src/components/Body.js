d343fa59cd1c8ed0e9180f4e5c85641791fed7f1
Abhishek Joshi <allabhishekjoshi.com>
2023-10-25 11:31:21 +0530

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,20 @@
 import ResturantCard from "./ResturantCard";
-import resList from "../utils/mockdata";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 const Body = () => {
 
-  const [listOfResturant, setlistOfResturant] = useState(resList);
+  const [listOfResturant, setlistOfResturant] = useState([]);
 
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
+  const fetchdata = async () =>{
+    const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=30.3826105&lng=78.0886934");
+    const json = await data.json();
+    console.log(json);
+    setlistOfResturant(json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+  }
   return (
     <>
       <div className="body">
