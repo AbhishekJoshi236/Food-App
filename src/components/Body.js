fcca1fe67a4e13886a8706217cb9ce297396f3a7
Abhishek Joshi <allabhishekjoshi.com>
2023-10-25 09:53:55 +0530

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,33 @@
 import ResturantCard from "./ResturantCard";
 import resList from "../utils/mockdata";
-
+import { useState } from "react";
 
 const Body = () => {
+
+  const [listOfResturant, setlistOfResturant] = useState(resList);
+
   return (
     <>
       <div className="body">
         <div className="filter">
-          <button className="filter-btn" onClick={() => {
+          <button 
+          className="filter-btn" 
+          onClick={() => {
             
-          }}>Top rated Resturant</button>
+            const filteredList = listOfResturant.filter( 
+              (res)=> res.info.avgRating >= 4.2
+            )
+            console.log(filteredList.length); //delete it
+            setlistOfResturant(filteredList);            
+          }}>
+            
+            Top rated Resturant</button>
         </div>
+
+
         <div className="res-container">
           {
-            resList.map((resturant) =>
+            listOfResturant.map((resturant) =>
               <ResturantCard key={resturant.info.id} resData={resturant} />)
           }
         </div>
