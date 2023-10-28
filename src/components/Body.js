a2263708793050ee967ebd50058e545d91dab7be
Abhishek Joshi <allabhishekjoshi.com>
2023-10-28 19:42:37 +0530

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
 import ResturantCard from "./ResturantCard";
 import { useState,useEffect } from "react";
-
+import Shimmer from "./Shimmer";
 const Body = () => {
 
   const [listOfResturant, setlistOfResturant] = useState([]);
@@ -10,10 +10,18 @@ const Body = () => {
   }, []);
 
   const fetchdata = async () =>{
-    const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=30.3826105&lng=78.0886934");
+    const data = await fetch(
+      'https://www.swiggy.com/mapi/homepage/getCards?lat=30.3826105&lng=78.0886934',
+    );
     const json = await data.json();
     console.log(json);
     setlistOfResturant(json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+    // setlistOfResturant(json.data.success.cards[1].gridWidget.gridElements.infoWithStyle.restaurants);
+    
+  }
+
+  if(listOfResturant.length === 0){
+    return <Shimmer />;
   }
   return (
     <>
