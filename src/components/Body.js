4f3aaea06b1a7d72baf9835fea7412faa99d3bc4
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2023-10-30 09:53:25 +0530

    Revert "New branch"

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,71 +1,56 @@
-import RestaurantCard from "./RestaurantCard";
+import ResturantCard from "./ResturantCard";
 import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
 const Body = () => {
 
-  const [listOfRestaurant, setlistOfRestaurant] = useState([]);
-  const[filterRestaurant, setFilterRestaurant] = useState(listOfRestaurant);
-  const[searchText, setSearchText] = useState("");
+  const [listOfResturant, setlistOfResturant] = useState([]);
 
   useEffect(() => {
-    console.log("effect working");
     fetchdata();
   }, []);
 
   const fetchdata = async () =>{
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=29.4501986&lng=77.3172046&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+      'https://www.swiggy.com/mapi/homepage/getCards?lat=30.3826105&lng=78.0886934',
     );
     const json = await data.json();
-    
-
-    console.log("api data" , json?.data?.cards);
-    if(json?.data?.cards.length === 13){
-      setlistOfRestaurant(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-      
-    }
-    else{
-
-      setlistOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    }
-    
+    console.log(json);
+    setlistOfResturant(json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
     // setlistOfResturant(json.data.success.cards[1].gridWidget.gridElements.infoWithStyle.restaurants);
     
   }
-  // if(listOfResturant.length === 0){
-  //   return <Shimmer />;
-  // }
+
+  if(listOfResturant.length === 0){
+    return <Shimmer />;
+  }
   return (
     <>
-    <div className="body">
+      <div className="body">
         <div className="filter">
-          
+          <button 
+          className="filter-btn" 
+          onClick={() => {
             
-            <button onClick={() => {
-              
-              
-              const filterRestaurant = listOfRestaurant.filter(restaurant => restaurant.info.name.toLowerCase().includes(searchText.toLowerCase()));
-              //setList(filterRestaurant);
-              setFilterRestaurant(filterRestaurant);
-            }}>Search Button</button>
-          </div>
-          
-          
-            <button 
-            >Top Rated Restaurant</button>
+            const filteredList = listOfResturant.filter( 
+              (res)=> res.info.avgRating >= 4.2
+            )
+            console.log(filteredList.length); //delete it
+            setlistOfResturant(filteredList);            
+          }}>
+            
+            Top rated Resturant</button>
         </div>
+
+
         <div className="res-container">
           {
-            
-            listOfRestaurant.map((restaurant) => <RestaurantCard key={restaurant.info.id} resList={restaurant}/>)
-            // console.log("list of restaurant", listOfRestaurant)
-
-          }  
-           
-            
+            listOfResturant.map((resturant) =>
+              <ResturantCard key={resturant.info.id} resData={resturant} />)
+          }
         </div>
+      </div>
     </>
-)
+  );
 };
 
 export default Body;
