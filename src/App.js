1afd5c970fa877a80efe3a1ce421ab359254831f
Abhishek Joshi <allabhishekjoshi.com>
2023-10-31 15:05:11 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-   
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error.js";
+
+
 const AppLayout = () => {
   return (
     <div className="app">
-        {/* header */}
         <Header />
-
-        {/* body */}
         <Body />
-        {/* footer */}
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
