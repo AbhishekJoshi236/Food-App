eab0fa55921976b457f82de1757c59be709011a0
Abhishek Joshi <allabhishekjoshi.com>
2023-11-10 20:34:07 +0530

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,5 @@
-import ResturantCard from "./ResturantCard";
-import { useState,useEffect } from "react";
+import ResturantCard, { withPromotedLabel } from "./ResturantCard";
+import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus.js";
@@ -12,23 +12,25 @@ const Body = () => {
   const [filteredRestaurant, setFilteredRestaurant] = useState([]);
   const [searchText, setSearchText] = useState("");
 
+  const ResturantCardPromoted = withPromotedLabel(ResturantCard);
+
   useEffect(() => {
     fetchdata();
   }, []);
 
-  const fetchdata = async () =>{
-    try{
+  const fetchdata = async () => {
+    try {
       const data = await fetch(
         'https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.3164945&lng=78.03219179999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING',
       );
       const json = await data.json();
-      console.log("json");
-      console.log(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-      
-      setlistOfResturant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);    
-      setFilteredRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants); 
-    
-    }catch(error){
+      // console.log("json");
+      // console.log(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+      setlistOfResturant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setFilteredRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+    } catch (error) {
       console.log("Error");
     }
   }
@@ -38,57 +40,61 @@ const Body = () => {
   }
 
   const onlineStatus = useOnlineStatus();
-  if(onlineStatus === false){
+  if (onlineStatus === false) {
     return <h1>Internet Connection Interupted!!</h1>
   }
-  
+
   return (
     <>
       <div className="body">
         <div className="flex">
           <div className="search m-4 p-4 " >
-            <input type="text" className="border-black border rounded-sm" value={searchText} 
-            onChange={(e)=>{
-              setSearchText(e.target.value);
-            }} />
-            
-            <button  className="px-4 py-0.9 m-4 bg-green-300 rounded-sm"
-              onClick={()=>{
-              
-              const filteredRestaurant = listOfResturant.filter(
-                (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase() )
-              );
+            <input type="text" className="border-black border rounded-sm" value={searchText}
+              onChange={(e) => {
+                setSearchText(e.target.value);
+              }} />
 
-              setFilteredRestaurant(filteredRestaurant);
+            <button className="px-4 py-0.9 m-4 bg-green-300 rounded-sm"
+              onClick={() => {
 
-            }}>Search</button>
+                const filteredRestaurant = listOfResturant.filter(
+                  (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                );
+
+                setFilteredRestaurant(filteredRestaurant);
+
+              }}>Search</button>
 
           </div>
           <div className="m-4 p-4 flex items-center">
-            <button 
-            className="px-4 py-0.9 m-4 bg-gray-300 rounded-sm" 
-            onClick={() => {
-              
-              console.log("top rated clicked");
-              const filteredList = listOfResturant.filter( 
-                (res)=> res.info.avgRating >= 4.0
-              ) 
-              setFilteredRestaurant(filteredList);            
-            }}>
-              
+            <button
+              className="px-4 py-0.9 m-4 bg-gray-300 rounded-sm"
+              onClick={() => {
+
+                console.log("top rated clicked");
+                const filteredList = listOfResturant.filter(
+                  (res) => res.info.avgRating >= 4.0
+                )
+                setFilteredRestaurant(filteredList);
+              }}>
+
               Top rated Resturant
             </button>
           </div>
-          
+
         </div>
 
         <div className="flex flex-wrap place-content-evenly">
-          {  
-            filteredRestaurant.map( (res) => {
-              return <Link 
+          {
+            filteredRestaurant.map((res) => {
+              return <Link
                 key={res.info.id}
-                to={"/restaurants/" +res.info.id} style={{ color: 'inherit', textDecoration: 'none'}}> 
-                <ResturantCard  resData={res} /> 
+                to={"/restaurants/" + res.info.id} style={{ color: 'inherit', textDecoration: 'none' }}>
+                {
+                  res.info.aggregatedDiscountInfoV3 ? <ResturantCardPromoted resData={res} /> : <ResturantCard resData={res} />
+                }
+                
+                
               </Link>
             })
           }
--- src/components/ResturantCard.js
+++ src/components/ResturantCard.js
@@ -5,17 +5,32 @@ const ResturantCard = (props) =>{
   const {cloudinaryImageId,name,cuisines,costForTwo,avgRating,sla} = resData?.info; 
   return(
     <>
-    <div className="m-3 p-3 w-[250px] rounded-lg shadow-xl h-50 bg-gray-100 hover:bg-gray-200" >
+    <div className="m-3 p-3 w-[250px] rounded-lg shadow-xl h-50 bg-gray-100 hover:bg-gray-200 shadow-inner" >
       <img className="rounded-lg h-[200px] m-auto" src={CDN_URL+cloudinaryImageId } />
       <h3 className="font-bold py-2 text-lg">{name}</h3>
       <h3>{cuisines.join(", ")}</h3>
-      <h4>{avgRating} stars</h4>
+      <h4>{"⭐️"} {avgRating}</h4>
       <h4>{costForTwo}</h4>
       <h4>{sla.deliveryTime} mins</h4>
     </div>
     </>
   );
 };
+
+// Higher Order Component
+export const withPromotedLabel = (ResturantCard) =>{
+  return (props) => {
+    const {resData} = props;  
+    
+    const meta = resData.info.aggregatedDiscountInfoV3;
+    return <div>
+      <label className="absolute bg-black text-white rounded-lg p-2 font-semibold">
+         {meta.header} {meta.subHeader}
+      </label>
+       <ResturantCard {...props}/> 
+    </div>
+  }
+}
 
 export default ResturantCard;
  
