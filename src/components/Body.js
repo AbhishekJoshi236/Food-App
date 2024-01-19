d64aaf4e9546ef3be09aef3b7f7524d6c3e7ac69
Abhishek Joshi <allabhishekjoshi.com>
2024-01-19 09:51:41 +0530

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -22,14 +22,15 @@ const Body = () => {
   const fetchdata = async () => {
     try {
       const data = await fetch(
+        // 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.3164945&lng=78.03219179999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING',
         'https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.3164945&lng=78.03219179999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING',
       );
       const json = await data.json();
 
-      setlistOfResturant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-      setFilteredRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setlistOfResturant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
       setBestOffer(json?.data?.cards[0]?.card?.card?.imageGridCards?.info);
-      // console.log(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      // console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
       // console.log(json?.data?.cards[0]?.card?.card?.imageGridCards?.info);
     } catch (error) {
       console.log("Error");
